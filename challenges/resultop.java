2dbc02e0e9daa6579649aaaddeabd076c98853ec
pranjulchaturvedi007 <pranjulchaturvedi561@gmail.com>
2025-03-20 21:51:53 +0530

    Ready to be commited

---
+++ challenges/resultop.java
@@ -0,0 +1,14 @@
+package challenges;
+
+public class resultop {
+    public static void main(String[] args) {
+        // Ouestion - Create a program that taken two numbers and shows result of all arithmatic operators
+        int a = 144;
+        int b = 12;
+    System.out.println(a/b);
+    int a1 = 1332;
+    int a2 = 8847;
+    System.out.println(a1-a2);
+    }
+
+}
